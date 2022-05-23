00f302e8914b820dc39a1845e2c76ed259c18e60
Andrey Zhygarev <infernalsteel666@gmail.com>
2022-05-23 22:13:27 +0300

    complete and error functions for observer

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import { Observable } from "rxjs";
 
 const observable = new Observable((subscriber) => {
-  subscriber.next("hello world");
+//   subscriber.next("hello world");
+//   subscriber.error("this is error");
+//   subscriber.next("hello rxjs");
+
+//   subscriber.complete();
+
+//   subscriber.next("hello again rxjs");
 });
 
 observable.subscribe({
   next: (value) => {
     console.log(value);
+  },
+  complete: () => {
+    console.log("complete called!");
+  },
+  error: (error) => {
+    console.error("ERROR ---> ", error);
   },
 });
