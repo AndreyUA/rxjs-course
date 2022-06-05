7d52a7ab6a759f22416449423262b87fd5bca65f
AndreyZhygarev <zhygarev.andrey.dev@gmail.com>
2022-06-05 19:46:27 +0300

    fromEvent operator

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import { interval, timer } from "rxjs";
+import { fromEvent } from "rxjs";
 
-const observableInterval = interval(1000);
+const observable = fromEvent(document, "click");
 
-const subscriptionInterval = observableInterval.subscribe(console.log);
-
-const observableTimer = timer(0, 1000);
-
-const subscriptionTimer = observableTimer.subscribe(console.log);
+const subscription = observable.subscribe(console.log);
