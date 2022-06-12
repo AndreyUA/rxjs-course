d8709d7f73752e6aba73a51a3e9dbade56196f61
AndreyZhygarev <zhygarev.andrey.dev@gmail.com>
2022-06-12 15:35:14 +0300

    pluck operator

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import { of } from "rxjs";
-import { map } from "rxjs/operators";
+import { fromEvent } from "rxjs";
+import { map, pluck } from "rxjs/operators";
 
-const observable = of(1, 2, 3, 4, 5);
-
-const numbersWithSymbol = observable.pipe(map((value) => `$${value}`));
+const observable = fromEvent(document, "keydown").pipe(
+  // map((event) => event.code)
+  pluck("code")
+);
 
 const subscription = observable.subscribe({
   next(value) {
