1e50fde3da794863f19d6da1363b345ee5e73766
AndreyZhygarev <zhygarev.andrey.dev@gmail.com>
2022-06-12 16:57:36 +0300

    mergeMap operator

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import { of, from, fromEvent, interval } from "rxjs";
-import { map, pluck, filter, reduce, take, scan, tap } from "rxjs/operators";
+import {
+  map,
+  pluck,
+  filter,
+  reduce,
+  take,
+  scan,
+  tap,
+  mergeMap,
+} from "rxjs/operators";
 import { ajax } from "rxjs/ajax";
 
 const button = document.getElementById("btn");
 
 const observable = fromEvent(button, "click").pipe(
-  map(() => {
-    return ajax.getJSON("https://jsonplaceholder.typicode.com/todos/1");
-  })
+  mergeMap(
+    () => interval(1000).pipe(tap(console.log), take(6))
+    // ajax.getJSON("https://jsonplaceholder.typicode.com/todos/1")
+  )
 );
-
 const subscription = observable.subscribe({
   next(value) {
-    value.subscribe(console.log);
+    console.log(value);
   },
   complete() {
     console.log("completed!");
